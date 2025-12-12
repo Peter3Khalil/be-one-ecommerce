4f4df930c10e638ebc8d241868539a148e8ffbb1
Peter3Khalil <peter.khalil.fahmy@gmail.com>
2025-12-12 23:15:54 +0200

    feat: enhance header responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { CircleUserRound, ShoppingCart } from 'lucide-react';
+import { CircleUserRound, Menu, Search, ShoppingCart } from 'lucide-react';
 import Link from 'next/link';
 
 const Header = () => {
@@ -12,11 +12,21 @@ const Header = () => {
   return (
     <header className="border-b py-4">
       <div className="container flex items-center justify-between gap-8">
-        <nav className="flex items-center gap-8">
-          <Link href="/" className="text-3xl leading-none font-bold">
+        <nav className="flex items-center sm:gap-8">
+          <Button
+            variant="ghost"
+            className="me-1 text-muted-foreground duration-200 hover:text-foreground sm:hidden"
+            size="icon"
+          >
+            <Menu />
+          </Button>
+          <Link
+            href="/"
+            className="text-3xl leading-none font-bold text-nowrap"
+          >
             Be One.
           </Link>
-          <ul className="flex items-center gap-4">
+          <ul className="hidden items-center gap-4 sm:flex">
             {NAV_ITEMS.map((item) => (
               <li
                 key={item.href}
@@ -32,9 +42,16 @@ const Header = () => {
           <Input
             type="search"
             placeholder="Search products..."
-            className="rounded-full border px-4 py-2 lg:w-80"
+            className="hidden rounded-full border px-4 py-2 md:block lg:w-80"
           />
           <div className="shrink-0">
+            <Button
+              variant="ghost"
+              className="text-muted-foreground duration-200 hover:text-foreground md:hidden"
+              size="icon"
+            >
+              <Search />
+            </Button>
             <Button
               variant="ghost"
               className="text-muted-foreground duration-200 hover:text-foreground"
