0140669d3291fa6679d91d6316e0c6963b6df043
Peter3Khalil <peter.khalil.fahmy@gmail.com>
2025-12-13 06:44:06 +0200

    feat: add Top Selling component with product listings and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@components/hero';
 import NewArrivals from '@components/new-arrivals';
+import TopSelling from '@components/top-selling';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <NewArrivals />
+      <TopSelling />
     </div>
   );
 };
