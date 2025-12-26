d599cb5d63ab75e11ef438e1170569fae76eb7c9
Peter3Khalil <peter.khalil.fahmy@gmail.com>
2025-12-26 11:22:29 +0200

    feat(i18n): integrate eslint-plugin-i18next for improved localization linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,49 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import i18next from 'eslint-plugin-i18next';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // no vars unused
+  i18next.configs['flat/recommended'],
   {
     rules: {
       'no-unused-vars': ['error'],
+      'i18next/no-literal-string': [
+        'error',
+        {
+          mode: 'jsx-only',
+          'jsx-attributes': {
+            includes: ['label', 'title'],
+            exclude: [
+              'className',
+              'styleName',
+              'alt',
+              'lang',
+              'dir',
+              'href',
+              /^on-/,
+              'variant',
+              'size',
+              'color',
+              'attribute',
+              'role',
+              'style',
+              'type',
+              'key',
+              'id',
+              'width',
+              'height',
+              /^data-/,
+              /^aria-/,
+            ],
+          },
+        },
+      ],
     },
   },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
 export default eslintConfig;
