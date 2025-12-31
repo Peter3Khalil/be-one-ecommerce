5aa96c643d8d376b972f2991f6a5b8d94c26abdb
Peter3Khalil <peter.khalil.fahmy@gmail.com>
2025-12-31 04:01:25 +0200

    feat: update reviews structure and localization handling for improved user experience

--- src/modules/products/components/reviews.tsx
+++ src/modules/products/components/reviews.tsx
@@ -1,15 +1,19 @@
 import { reviews } from '@public/data.json';
 import { Button } from '@ui/button';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import ReviewCard from './review-card';
 const Reviews = () => {
   const t = useTranslations();
+  const locale = useLocale();
+  const reviewsList = reviews[locale] || reviews['en'];
   return (
     <section className="container py-10 md:py-16">
       <div className="mb-8 flex items-center justify-between">
         <h2 className="text-2xl font-bold md:text-3xl">
           {t('Global.customerReviews')}
-          <span className="ml-2 text-muted-foreground">({reviews.length})</span>
+          <span className="ml-2 text-muted-foreground">
+            ({reviewsList.length})
+          </span>
         </h2>
         <Button className="rounded-full" size="lg">
           {t('Global.writeReview')}
@@ -17,7 +21,7 @@ const Reviews = () => {
       </div>
 
       <div className="grid gap-6 sm:grid-cols-2">
-        {reviews.map((review) => (
+        {reviewsList.map((review) => (
           <ReviewCard key={review.id} {...review} />
         ))}
       </div>
